adcc613ed7376c84693efc67b8e8c7010b3e3b4a
Jakub Sajnóg <jakubsajnog@gmail.com>
2024-03-22 13:21:51 +0100

    add Tests solution

--- zadanie/LegacyApp.Tests/UserServiceTest.cs
+++ zadanie/LegacyApp.Tests/UserServiceTest.cs
@@ -1,3 +1,4 @@
+using System;
 using JetBrains.Annotations;
 using LegacyApp;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -10,8 +11,12 @@ public class UserServiceTest
 {
 
     [TestMethod]
-    public void METHOD()
+    public void AddUser_Sould_Return_False_When_FirstName_Is_Missing()
     {
+        var userService = new UserService();
+
+        var addResult = userService.AddUser("", "Kowalski", "kowalski@wp.pl", DateTime.Parse("1980-12-12"), 1);
         
+        Assert.IsFalse(addResult);
     }
 }
---
+++ zadanie/LegacyApp_.Tests/UserServiceTest.cs
@@ -0,0 +1,16 @@
+using JetBrains.Annotations;
+using LegacyApp;
+using Xunit;
+
+namespace LegacyApp_.Tests;
+
+[TestSubject(typeof(UserService))]
+public class UserServiceTest
+{
+
+    [Fact]
+    public void METHOD()
+    {
+        
+    }
+}
