d404cee21fd66209a8f197c05a9c27cde29bc6a9
Jakub Sajnóg <jakubsajnog@gmail.com>
2024-03-22 12:48:14 +0100

    add Tests solution

---
+++ zadanie/LegacyApp.Tests/UserServiceTest.cs
@@ -0,0 +1,17 @@
+using JetBrains.Annotations;
+using LegacyApp;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LegacyApp.Tests;
+
+[TestClass]
+[TestSubject(typeof(UserService))]
+public class UserServiceTest
+{
+
+    [TestMethod]
+    public void METHOD()
+    {
+        
+    }
+}
